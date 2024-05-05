6a281f9f677047a7cb8964fa502304de6297b8b4
rpungello <rpungello@users.noreply.github.com>
2024-05-05 20:41:41 +0000

    Fix styling

--- database/seeders/TeamTypeSeeder.php
+++ database/seeders/TeamTypeSeeder.php
@@ -9,7 +9,7 @@ class TeamTypeSeeder extends Seeder
 {
     public function run(): void
     {
-        foreach($this->getDefaultTypes() as $name) {
+        foreach ($this->getDefaultTypes() as $name) {
             TeamType::create(['name' => $name]);
         }
     }
