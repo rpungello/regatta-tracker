5f64140be3ec6321f3db75d99aadf9fe87539caa
Rob Pungello <rob.pungello@gmail.com>
2024-05-09 07:30:55 -0400

    Make brand link to regattas

--- resources/views/components/app-brand.blade.php
+++ resources/views/components/app-brand.blade.php
@@ -1,12 +1,10 @@
-<a href="/" wire:navigate>
+<a href="{{ route('regattas.list') }}" wire:navigate>
     <!-- Hidden when collapsed -->
     <div {{ $attributes->class(["hidden-when-collapsed"]) }}>
         <div class="flex items-center gap-2">
             <x-icon name="o-square-3-stack-3d" class="w-6 -mb-1 text-purple-500"/>
             <span
-                class="font-bold text-3xl mr-3 bg-gradient-to-r from-purple-500 to-pink-300 bg-clip-text text-transparent ">
-                                {{ config('app.name') }}
-                            </span>
+                class="font-bold text-3xl mr-3 bg-gradient-to-r from-purple-500 to-pink-300 bg-clip-text text-transparent ">{{ config('app.name') }}</span>
         </div>
     </div>
 
