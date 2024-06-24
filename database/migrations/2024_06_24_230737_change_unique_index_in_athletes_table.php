641c7c64d32a5e00bec804bddc220535f545c227
Rob Pungello <rob.pungello@gmail.com>
2024-06-24 19:10:24 -0400

    Allow the same athlete name across teams

---
+++ database/migrations/2024_06_24_230737_change_unique_index_in_athletes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::table('athletes', function (Blueprint $table) {
+            $table->dropUnique([
+                'name_first',
+                'name_last'
+            ]);
+            $table->unique([
+                'team_id',
+                'name_last',
+                'name_first',
+            ]);
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('athletes', function (Blueprint $table) {
+            $table->dropUnique([
+                'team_id',
+                'name_last',
+                'name_first'
+            ]);
+            $table->unique([
+                'name_first',
+                'name_last',
+            ]);
+        });
+    }
+};
