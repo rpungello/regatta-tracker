68cd407b50ee29febc7f9a6c46a5041046d326bf
rpungello <rpungello@users.noreply.github.com>
2024-06-24 23:11:00 +0000

    Fix styling

--- database/migrations/2024_06_24_230737_change_unique_index_in_athletes_table.php
+++ database/migrations/2024_06_24_230737_change_unique_index_in_athletes_table.php
@@ -4,13 +4,14 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     public function up(): void
     {
         Schema::table('athletes', function (Blueprint $table) {
             $table->dropUnique([
                 'name_first',
-                'name_last'
+                'name_last',
             ]);
             $table->unique([
                 'team_id',
@@ -26,7 +27,7 @@ return new class extends Migration {
             $table->dropUnique([
                 'team_id',
                 'name_last',
-                'name_first'
+                'name_first',
             ]);
             $table->unique([
                 'name_first',
