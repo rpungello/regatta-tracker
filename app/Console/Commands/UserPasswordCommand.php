d754226368a3dc02ee7da2fb70b8d4381df93a77
Rob Pungello <rob.pungello@gmail.com>
2024-11-02 19:12:09 -0400

    Add command to update user passwords

---
+++ app/Console/Commands/UserPasswordCommand.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Facades\Hash;
+use function Laravel\Prompts\password;
+
+class UserPasswordCommand extends Command
+{
+    protected $signature = 'user:password {user}';
+
+    protected $description = 'Resets the password for a given user';
+
+    public function handle(): int
+    {
+        $userArgument = $this->argument('user');
+        try {
+            if (is_numeric($userArgument)) {
+                $user = User::findOrFail($userArgument);
+            } else {
+                $user = User::whereEmail($userArgument)->firstOrFail();
+            }
+        } catch (ModelNotFoundException) {
+            $this->error('User not found');
+
+            return static::FAILURE;
+        }
+
+        $user->update([
+            'password' => Hash::make(password('Enter new password')),
+        ]);
+
+        $this->info('Password updated successfully');
+        return static::SUCCESS;
+    }
+}
