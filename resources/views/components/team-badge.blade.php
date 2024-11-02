e23ad9f2fd07da4f40ecf6f75e570150d2fb9e8f
Rob Pungello <rob.pungello@gmail.com>
2024-11-02 19:24:49 -0400

    Add per-boat priority highlighting

--- resources/views/components/team-badge.blade.php
+++ resources/views/components/team-badge.blade.php
@@ -3,7 +3,7 @@
     'entry',
 ])
 
-<div class="rounded-xl px-2.5 py-1.5 space-x-1 flex flex-row items-center transition-opacity @if(isset($entry) && $entry->complete) opacity-40 @endif"
+<div class="@if(isset($entry) && $entry->priority === \App\Enums\Priority::High) border-2 border-red-600 @endif rounded-xl px-2.5 py-1.5 space-x-1 flex flex-row items-center transition-opacity @if(isset($entry) && $entry->complete) opacity-40 @endif"
      style="background-color: #{{ $team->brand_color_primary }}; color: #{{ $team->brand_color_secondary }};">
     @if(isset($entry))
         <span class="mr-2">{{ $entry->bow_number }}</span>
@@ -19,7 +19,15 @@
     </div>
 
     @isset($entry)
-        <x-icon name="s-check-badge" :class="$entry->complete === false ? 'opacity-0' : ''" />
+        @if($entry->complete)
+            <x-icon name="s-check-badge" />
+        @elseif($entry->priority === \App\Enums\Priority::High)
+            <x-icon name="s-star" />
+        @elseif ($entry->priority === \App\Enums\Priority::Low)
+            <x-icon name="s-chevron-double-down" />
+        @else
+            <div class="w-5"></div>
+        @endif
     @endisset
 
 </div>
