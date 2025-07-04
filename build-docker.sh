5f75faa8b39636acc7a94098f5882055e485295d
Rob Pungello <rob.pungello@gmail.com>
2025-07-04 17:32:26 -0400

    Move images to Github packages

--- build-docker.sh
+++ build-docker.sh
@@ -2,43 +2,15 @@
 
 set -e
 
-# Make sure we have the required commands to run this script
-if ! command -v jq &>/dev/null; then
-    echo "jq is required to run this script"
-    exit 1
-fi
-
-if ! command -v semver &>/dev/null; then
-    echo "semver is required to run this script"
-    exit 1
-fi
-
 # Read the current version from composer.json
-CURRENT_VERSION=$(jq -r ".version" composer.json)
-
-# Read the increment argument (if provided)
-INCREMENT=$1
-
-# If $INCREMENT is not empty, run semver to increment the version, then update composer.json and create a git tag
-if [ -n "$INCREMENT" ]; then
-    VERSION=$(semver --increment "$INCREMENT" "$CURRENT_VERSION")
-    jq ".version=\"$VERSION\"" --indent 4 composer.json >composer.json.new
-    mv composer.json.new composer.json
-
-    git add composer.json
-    git commit -qm "Bump version to $VERSION"
-    git tag -a -m "Tagging version $VERSION" "$VERSION"
-else
-    VERSION=$CURRENT_VERSION
-fi
+CURRENT_VERSION=$(git describe --tags --abbrev=0)
 
 # Build the Docker image
-echo "Building Regatta Tracker v$VERSION"
+echo "Building Regatta Tracker v$CURRENT_VERSION"
 docker buildx build \
        --pull \
        --no-cache \
-       --build-arg VERSION="$VERSION" \
-       --platform=linux/amd64,linux/arm64/v8 \
-       --tag "public.ecr.aws/rpungello/regatta-tracker:${VERSION}" \
-       --tag "public.ecr.aws/rpungello/regatta-tracker:latest" \
-       --push .
+       --build-arg VERSION="$CURRENT_VERSION" \
+       --tag "rpungello/regatta-tracker:latest" \
+       --tag "regatta-tracker:latest" \
+       --load .
